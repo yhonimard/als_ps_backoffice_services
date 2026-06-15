628ce767af49c8daa4dc1ec241cb0d8d88973978
Yhonimard <yhoni2103@gmail.com>
2026-06-15 20:25:52 +0700

    feat/fix

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,6 +1,6 @@
 import "dotenv/config"
 import { PrismaPg } from "@prisma/adapter-pg"
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "../prisma/generated/index"
 
 
 const connectionString = process.env.DATABASE_URL
